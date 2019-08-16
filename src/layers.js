20d966cd3eac6644b10a75730cf9db1171e2ed51
toonnyy8 <toonnyy8@gmail.com>
2019-08-16 12:30:17 +0800

    LayerNormalization

--- src/layers.js
+++ src/layers.js
@@ -1,41 +1,69 @@
 import * as tf from "@tensorflow/tfjs"
 
+
 export class LayerNormalization extends tf.layers.Layer {
-    constructor(init = { axis }) {
-        super({})
-        this.supportsMasking = true
-        this.axis = init.axis || -1
+    constructor(args) {
+        super({});
+        this.axis = args.axis;
+        //console.log("LayerNorm constructor");
+        //console.log(this.axis);
     }
-
-    apply(inputs, kwargs) {
-        let input = inputs
+    computeCorrectName(name) {
+        //layer_norm_LayerNorm1/g  must be converted to layer_norm/g
+        //layer_norm_LayerNorm2/g  must be converted to layer_norm_1/g
+        let prefix = "layer_norm_LayerNorm";
+        let subsr = name.substr(prefix.length);
+        let vals = subsr.split('/');
+        let num = parseInt(vals[0]);
+        let nbstr = num == 1 ? "" : "_" + (num - 1).toString();
+        let res = "layer_norm" + nbstr + "/" + vals[1];
+        return res;
+    }
+    build(inputShape) {
+        //console.log("LayerNorm build : ")
+        this.g = this.addWeight("g", [inputShape[inputShape.length - 1]], "float32", tf.initializers.ones());
+        this.b = this.addWeight("b", [inputShape[inputShape.length - 1]], "float32", tf.initializers.zeros());
+        let gname = this.computeCorrectName(this.g.originalName);
+        let bname = this.computeCorrectName(this.b.originalName);
+        this.g.originalName = gname;
+        this.g.name = gname;
+        this.b.originalName = bname;
+        this.b.name = bname;
+        //console.log(this.g);
+        //console.log(this.b);
+        this.built = true;
+    }
+    computeOutputShape(inputShape) {
+        //console.log("LayerNorm computeOutputShape");
+        //console.log(inputShape);
+        return inputShape;
+    }
+    call(inputs, kwargs) {
+        let input = inputs;
         if (Array.isArray(input)) {
-            input = input[0]
+            input = input[0];
         }
-        this.invokeCallHook(inputs, kwargs)
-
-        let x = input
-        let epsilon = 1e-5
-        let axis = this.axis == -1 ? input.shape.length - 1 : this.axis
-        let u = tf.mean(x, axis, true)
-        let xmu = tf.sub(x, u)
-        let s = tf.mean(tf.square(xmu), axis, true)
-        x = tf.mul(xmu, tf.rsqrt(tf.add(s, epsilon)))
-        //let gval = this.g.read()
-        let gval = tf.reshape(this.g.read(), [1, 1, -1])
-        let bval = tf.reshape(this.b.read(), [1, 1, -1])
-        //x = x * + tf.reshape( this.b,[1,1,-1])
-        x = tf.add(tf.mul(x, gval), bval)
-
-        return x
-
+        //console.log("LayerNorm call");
+        this.invokeCallHook(inputs, kwargs);
+        let x = input;
+        let epsilon = 1e-5;
+        let axis = this.axis == -1 ? input.shape.length - 1 : this.axis;
+        let u = tf.mean(x, axis, true);
+        let xmu = tf.sub(x, u);
+        let s = tf.mean(tf.square(xmu), axis, true);
+        x = tf.mul(xmu, tf.rsqrt(tf.add(s, epsilon)));
+        //let gval = this.g.read();
+        let gval = tf.reshape(this.g.read(), [1, 1, -1]);
+        let bval = tf.reshape(this.b.read(), [1, 1, -1]);
+        //x = x * + tf.reshape( this.b,[1,1,-1]);
+        x = tf.add(tf.mul(x, gval), bval);
+        return x;
     }
-
     /**
      * Layers must implement "getClassName".
      */
     getClassName() {
-        return 'LayerNormalization'
+        return "LayerNormalization"
     }
 }
 
